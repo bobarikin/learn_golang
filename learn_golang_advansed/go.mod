71b5606355d31988a2ab7586e8cac7ddb9700b78
Artyom <a.s.bobarikin@gmail.com>
2023-01-17 12:16:23 +0300

    add files

---
+++ learn_golang_advansed/go.mod
@@ -0,0 +1,5 @@
+module lear
+
+go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ learn_golang_advansed/internal/user/handler.go
@@ -0,0 +1,54 @@
+package user
+
+import (
+	"lear/internal/handlers"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+const (
+	usersURL = "/users"
+	userURL = "/users/:uuid"
+)
+
+type handler struct {
+	
+}
+
+func NewHandler() handlers.Handler {
+	return &handler{}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersURL, h.GetList)
+	router.POST(userURL, h.CreateUser)
+	router.GET(userURL, h.GetUserByUUID)
+	router.PUT(userURL, h.UpdateUser)
+	router.PATCH(userURL, h.PartiallyUpdateUser)
+	router.DELETE(userURL, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is list of users"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is create user"))
+}
+
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is get user by uuid"))
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is update"))
+}
+
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is partially update user"))
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is delete user"))
+}
